f63d70634f3b54ed69c2b2b56cb1e105c3afd9c9
TimofeevAndrei <i.pika4u@gmail.com>
2022-11-06 01:27:27 +0700

    read cook book

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-with open('files/cook_book.txt', 'rt', encoding="utf-8") as cook_book:
-    cb=cook_book.read()
-    print(cb)
+with open('files/cook_book.txt', 'rt', encoding="utf-8") as file:
+    cook_book = {}
+    for line in file:
+        dish = line.strip()
+        ingredients_count = int(file.readline())
+        structure = []
+        for _ in range(ingredients_count):
+            ing = file.readline().strip().split(' | ')
+            ingredients, quantity, measure = ing
+            structure.append({'ingredients': ingredients, 'quantity': quantity, 'measure': measure})
+        cook_book.update({dish:structure})
+        file.readline()
+    print(cook_book)
