205b1d6880022944cc2e5427c04090f7bfa62207
TimofeevAndrei <i.pika4u@gmail.com>
2022-11-06 01:54:48 +0700

    add in def

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-with open('files/recipes.txt', 'rt', encoding="utf-8") as file:
-    cook_book = {}
-    for line in file:
-        dish = line.strip()
-        ingredients_count = int(file.readline())
-        structure = []
-        for _ in range(ingredients_count):
-            ing = file.readline().strip().split(' | ')
-            ingredients, quantity, measure = ing
-            structure.append({'ingredients': ingredients, 'quantity': quantity, 'measure': measure})
-        cook_book.update({dish:structure})
-        file.readline()
+def cook_book():
+    with open('files/recipes.txt', 'rt', encoding="utf-8") as file:
+        cook_book = {}
+        for line in file:
+            dish = line.strip()
+            ingredients_count = int(file.readline())
+            structure = []
+            for _ in range(ingredients_count):
+                ing = file.readline().strip().split(' | ')
+                ingredients, quantity, measure = ing
+                structure.append({'ingredients': ingredients, 'quantity': quantity, 'measure': measure})
+            cook_book.update({dish:structure})
+            file.readline()
 
-print(cook_book)
 
 
